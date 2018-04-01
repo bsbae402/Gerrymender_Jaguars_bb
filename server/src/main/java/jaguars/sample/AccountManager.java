7c1b31f2133acfeec37c9285f2469994b399e8f7
Bongsung Bae <bongsung.bae@stonybrook.edu>
2018-04-01 12:55:10 -0700

    session practice

--- server/src/main/java/jaguars/sample/AccountManager.java
+++ server/src/main/java/jaguars/sample/AccountManager.java
@@ -4,6 +4,7 @@ import jaguars.user.MapProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,18 +13,13 @@ public class AccountManager {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    private HttpSession session;
+
     HashMap<String, Account> accounts;
 
     public AccountManager() {
         accounts = new HashMap<>();
-//        Account bongsung = new Account("bong", "sung123", "bs@gmail.com");
-//        Account roger = new Account("roger", "654321", "roger@abc.com");
-//        Account dummy1 = new Account("dummy1", "asdf", "asdf@asdf.com");
-//        Account dummy2 = new Account("dummy2", "qwer", "qwer@qwer.com");
-//        accounts.put(bongsung.getId(), bongsung);
-//        accounts.put(roger.getId(), roger);
-//        accounts.put(dummy1.getId(), dummy1);
-//        accounts.put(dummy2.getId(), dummy2);
     }
 
     public ArrayList<Account> getAllAccounts() {
@@ -31,5 +27,13 @@ public class AccountManager {
     }
     public Account getAccount(String accountId) {
         return accountRepository.findOne(accountId);
+    }
+
+    public void setActiveAccountSession(Account loginedAccount) {
+        session.setAttribute("ActiveAccount", loginedAccount);
+    }
+
+    public Account getActiveAccountSession() {
+        return (Account) session.getAttribute("ActiveAccount");
     }
 }
