8f8fead0d5061fcf2d5f316610dd13ef2f08733a
Bongsung Bae <bongsung.bae@stonybrook.edu>
2018-05-05 18:26:59 -0400

    simplified OH precincts geojson

--- server/src/main/java/jaguars/AppConstants.java
+++ server/src/main/java/jaguars/AppConstants.java
@@ -22,7 +22,7 @@ public class AppConstants {
     public static final String PATH_DISTRICT_GEOJSON_OH2010 = PATH_GEOJSONS + "/district_boundaries/OH_districts_2011_REVISED.json";
     public static final String PATH_DISTRICT_GEOJSON_NH2010 = PATH_GEOJSONS + "/district_boundaries/district_2010_NH_jaguars.json";
     public static final String PATH_DISTRICT_GEOJSON_WI2010 = PATH_GEOJSONS + "/district_boundaries/WI_district_2011_jaguars.json";
-    public static final String PATH_PRECINCT_GEOJSON_OH2010 = PATH_GEOJSONS + "/precinct_boundaries/OH_precincts_2010_fixed.json";
+    public static final String PATH_PRECINCT_GEOJSON_OH2010 = PATH_GEOJSONS + "/precinct_boundaries/OH_precincts_2010_fixed_v2.json";
     public static final String PATH_PRECINCT_GEOJSON_NH2010 = PATH_GEOJSONS + "/precinct_boundaries/NH_precincts_2010.json";
     public static final String PATH_PRECINCT_GEOJSON_WI2010 = PATH_GEOJSONS + "/precinct_boundaries/WI_wards_2010_jaguars_v2.json";
 
